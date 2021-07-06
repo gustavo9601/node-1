049f1f2cdbbc6b6c5eb2b02fcce355def5ac7500
gustavo9601 <tavo9601@gmail.com>
2021-07-05 23:41:54 -0500

    app console use axios

--- 05-app-console-consume-api-weather/index.js
+++ 05-app-console-consume-api-weather/index.js
@@ -18,22 +18,35 @@ const main = async () => {
                 const ciudad = await leerInput('Ciudad');
                 // Busqueda de lugares
                 const lugares = await busquedas.buscarCiudad(ciudad);
-                console.log("lugares", lugares)
                 // Seleccionar el lugar
                 const idCiudadSeleccionado = await listarLugares(lugares);
-                console.log("idCiudadSeleccionado", idCiudadSeleccionado)
+                // Si se selecciona 0 se skipea el resto de codigo
+                if (idCiudadSeleccionado == '0') continue;
+
                 const ciudadSeleccionada = lugares.find(lugar => lugar.id === idCiudadSeleccionado);
-                console.log("ciudadSeleccionada", ciudadSeleccionada)
+
+                //Guardar la busqueda en db
+                busquedas.agregarHistorial(ciudadSeleccionada.nombre);
 
                 // Clima
+                const clima = await busquedas.climaCiudad(ciudadSeleccionada.lat, ciudadSeleccionada.lon);
+                console.log("clima > ", clima);
+
                 // Mostrar resultados
                 console.log('\nInformacion de la ciudad\n'.green);
                 console.log('Ciudad:', ciudadSeleccionada.nombre);
                 console.log('Lat:', ciudadSeleccionada.lat);
-                console.log('Lng:', ciudadSeleccionada.lng);
-                console.log('Temperatura:');
-                console.log('Minima:');
-                console.log('Maxima:');
+                console.log('Lng:', ciudadSeleccionada.lon);
+                console.log('Temperatura:', clima.temp);
+                console.log('Minima:', clima.min);
+                console.log('Maxima:', clima.max);
+                console.log('Como esta el clima', clima.desc.green)
+                break;
+            case 2:
+                busquedas.historial.forEach((ciudad, i) => {
+                   const idx =  `${i + 1}.`.green;
+                   console.log(`${idx} ${ciudad}`);
+                });
                 break;
         }
 
--- 05-app-console-consume-api-weather/models/busquedas.js
+++ 05-app-console-consume-api-weather/models/busquedas.js
@@ -1,10 +1,13 @@
 const axios = require('axios');
+const fs = require('fs')
 
 class Busquedas {
-    historial = ['Bogota', 'Cali', 'Barranqulla', 'Nueva York']
+    historial = [];
+    dbPath = './db/database.json';
 
     constructor() {
         // to do leer db si existe
+        this.leerDB();
     }
 
     obtenerParametros() {
@@ -29,14 +32,77 @@ class Busquedas {
             return respuesta.data.features.map(lugar => ({
                 id: lugar.id,
                 nombre: lugar.place_name,
-                lng: lugar.center[0],
+                lon: lugar.center[0],
                 lat: lugar.center[1]
             }));
 
         } catch (error) {
             console.log("error buscarCiudad > ", error);
         }
+    }
 
+
+    obtenerParametrosWeather() {
+        // OPENWEATHER_API
+        return {
+            'appid': process.env.OPENWEATHER_API,
+            'units': 'metric',
+            'lang': 'es'
+        }
+    }
+
+    async climaCiudad(lat, lon) {
+        try {
+
+            // Configurando axios para realizar el request
+            const instance = axios.create({
+                baseURL: `https://api.openweathermap.org/data/2.5/weather`,
+                params: {...this.obtenerParametrosWeather(), lat, lon}
+            });
+            // realizando la peticion
+            const respuesta = await instance.get();
+            // creamos las variables, destructurando la respuesta.data;
+            const {weather, main} = respuesta.data;
+            /* => ({})  // significa que retornara un objeto de forma implicita */
+            // resúesta de la peticion esta en .data
+            return {
+                desc: weather[0].description,
+                min: main.temp_min,
+                max: main.temp_max,
+                temp: main.temp
+            };
+
+        } catch (error) {
+            console.log("error clima", error);
+        }
+    }
+
+    agregarHistorial(ciudad = '') {
+
+        const lugar = ciudad.toLowerCase();
+        if (this.historial.includes(lugar)) {
+            return;
+        }
+
+        this.historial.unshift(lugar);
+        this.guardarDB();
+    }
+
+    guardarDB() {
+        const payload = {
+            historial: this.historial
+        };
+        fs.writeFileSync(this.dbPath, JSON.stringify(payload));
+    }
+
+    leerDB() {
+        // verifica si el archivo existe
+        if (!fs.existsSync(this.dbPath)) return;
+
+        const dataFile = fs.readFileSync(this.dbPath, {encoding: 'utf-8'});
+        const data = JSON.parse(dataFile);
+
+        this.historial = data.historial;
     }
 
 }
